3a63855cbb050a5eadc4039d70582260fb204c18
GiladShmueli <33592872+GiladShmueli@users.noreply.github.com>
2020-11-18 14:16:14 +0200

    The Beginning
    It's the start of something beautiful

---
+++ main/java/com/example/deliveriesapp/DeliveriesActivity.java
@@ -0,0 +1,14 @@
+package com.example.deliveriesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deliveries);
+    }
+}
